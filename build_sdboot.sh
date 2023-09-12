1f314216623fdedfd5094fc7b110f194d60625b9
Ajay Iyengar <12970408+ajiyengar@users.noreply.github.com>
2023-09-12 07:22:27 -0700

    Generate disk with ESP and inject systmed-boot

--- build_sdboot.sh
+++ build_sdboot.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
-#Deps: pacman -S meson ninja pyelftools gperf
+#Deps sd-boot: pacman -S meson ninja pyelftools gperf
+#Deps esp: parted mtools
 
 set -e
 
-meson setup --default-library static --prefer-static --cross-file meson_aarch64.txt systemd/build_aarch64/ systemd/
-ninja -C systemd/build_aarch64/ systemd-boot
+#meson setup --default-library static --prefer-static --cross-file meson_aarch64.txt systemd/build_aarch64/ systemd/
+#ninja -C systemd/build_aarch64/ systemd-boot
+
+dd if=/dev/zero of=esp.img bs=512 count=93750
+parted esp.img -s -a minimal mklabel gpt
+parted esp.img -s -a minimal mkpart EFI FAT16 2048s 93716s
+parted esp.img -s -a minimal toggle 1 boot
+
+dd if=/dev/zero of=/tmp/part.img bs=512 count=91669
+mformat -i /tmp/part.img -h 32 -t 32 -n 64 -c 1
+
+mmd -i /tmp/part.img ::/EFI
+mmd -i /tmp/part.img ::/EFI/BOOT
+mcopy -i /tmp/part.img systemd/build_aarch64/src/boot/efi/systemd-bootaa64.efi ::/EFI/BOOT/BOOTAA64.efi
+
+dd if=/tmp/part.img of=esp.img bs=512 count=91669 seek=2048 conv=notrunc
 
 mkdir -p VirtualDrive
-cp systemd/build_aarch64/src/boot/efi/systemd-bootaa64.efi VirtualDrive/
+#cp systemd/build_aarch64/src/boot/efi/systemd-bootaa64.efi VirtualDrive/
+echo 'world' > VirtualDrive/hello.txt
--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-setsid ${TERMINAL} -e aarch64-linux-gnu-gdb -ex "target remote localhost:1234" &
+setsid ${TERMINAL} -e aarch64-linux-gnu-gdb \
+  -ex "target remote localhost:1234" \
+  -ex "source edk2/BaseTools/Scripts/efi_gdb.py" &
+
 qemu-system-aarch64 \
   -machine type=virt,virtualization=on,pflash0=rom,pflash1=efivars \
   -cpu max \
