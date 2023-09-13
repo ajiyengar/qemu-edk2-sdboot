1e778ad6d234e1d52f88a27c26e1ea60ef8d3355
Ajay Iyengar <12970408+ajiyengar@users.noreply.github.com>
2023-09-12 23:44:21 -0700

    Enable support for NVME Disk Drive containing EFI System Partition

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
-# UEFI + Systemd-Boot on QEMU
+# EDK2 ArmVirtPkg + Systemd-Boot + Linux on QEMU
 
 ## Setup
+One time init:
+```
+git submodule update --init --recursive
+```
+
+## EDK2
+Run `docker_build.sh` to build EDK2 ArmVirtPkg
+
+## Systemd
+Run `build_sdboot.sh` to generate systemd-bootaa64.efi
+
+## Linux
+
+## QEMU + GDB
+Edit `run.sh` and uncomment '-s -S' when starting QEMU to enable GDB breaking. Also uncomment remote GDB launching.
+Run `run.sh` to start QEMU
+
+---
+
+#### Repo Setup Details
 ```
 git submodule add https://github.com/systemd/systemd.git systemd
 git add systemd
 git submodule add https://github.com/tianocore/edk2.git edk2
 git add edk2
 ```
-
-## Systemd
-Run `build_sdboot.sh` to generate systemd-bootaa64.efi
 
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-setsid ${TERMINAL} -e aarch64-linux-gnu-gdb \
-  -ex "target remote localhost:1234" \
-  -ex "source edk2/BaseTools/Scripts/efi_gdb.py" &
+#setsid ${TERMINAL} -e aarch64-linux-gnu-gdb \
+#  -ex "target remote localhost:1234" \
+#  -ex "source edk2/BaseTools/Scripts/efi_gdb.py" &
 
 qemu-system-aarch64 \
   -machine type=virt,virtualization=on,pflash0=rom,pflash1=efivars \
@@ -10,9 +10,11 @@ qemu-system-aarch64 \
   -smp 4 \
   -blockdev node-name=rom,driver=file,filename=QEMU_EFI.raw,read-only=true \
   -blockdev node-name=efivars,driver=file,filename=QEMU_VARS.raw \
+  -drive file=disk.img,if=none,id=nvm \
+  -device nvme,serial=deadbeef,drive=nvm \
   -drive file=fat:rw:VirtualDrive,format=raw,media=disk \
   -serial mon:stdio \
   -net none \
-  -display none \
-  -s \
-  -S
+  -display none
+#  \ -s \
+#  -S
