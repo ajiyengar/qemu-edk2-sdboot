8dcb3bfe93457565e5fbb59e4e99c6e76cea17ce
Ajay Iyengar <12970408+ajiyengar@users.noreply.github.com>
2023-09-30 15:48:29 -0700

    debug: Decrease num smp to simplify debug with gdb

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ setsid ${TERMINAL} -e aarch64-linux-gnu-gdb \
 qemu-system-aarch64 \
   -machine type=virt,virtualization=on,gic-version=3,pflash0=rom,pflash1=efivars \
   -cpu max,pauth-impdef=on \
-  -smp 4 \
+  -smp 1 \
   -m 4096 \
   -blockdev node-name=rom,driver=file,filename=QEMU_EFI.raw,read-only=true \
   -blockdev node-name=efivars,driver=file,filename=QEMU_VARS.raw \
