7a475ea2a7acad1b4021160ed0c0af8782dce8a5
Ajay Iyengar <12970408+ajiyengar@users.noreply.github.com>
2023-09-30 19:40:43 -0700

    build: Enable NVMe driver in linux kernel and disable reduced logging

--- build_linux.sh
+++ build_linux.sh
@@ -12,9 +12,14 @@ make -C linux ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE LOCALVERSION="-ajay" virtc
 sed -i 's/# CONFIG_DEBUG_EFI is not set/CONFIG_DEBUG_EFI=y/g' linux/.config
 #Enable GDB scripts
 sed -i 's/# CONFIG_GDB_SCRIPTS is not set/CONFIG_GDB_SCRIPTS=y/g' linux/.config
+#GDB debug scripts prefer reduced debug to not be enabled
+sed -i 's/CONFIG_DEBUG_INFO_REDUCED=y/# CONFIG_DEBUG_INFO_REDUCED is not set\n# CONFIG_DEBUG_INFO_BTF is not set/g' linux/.config
 #Disable KASLR
 sed -i 's/CONFIG_RANDOMIZE_BASE=y/# CONFIG_RANDOMIZE_BASE is not set/g' linux/.config
-#sed -i 's/CONFIG_DEBUG_INFO_REDUCED=y/# CONFIG_DEBUG_INFO_REDUCED is not set/g' linux/.config
+#Enable NVMe
+sed -i 's/CONFIG_NVME_CORE=m/CONFIG_NVME_CORE=y/g' linux/.config
+sed -i 's/CONFIG_BLK_DEV_NVME=m/CONFIG_BLK_DEV_NVME=y/g' linux/.config
+
 make -C linux -j$(nproc) ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE LOCALVERSION="-ajay" Image modules scripts_gdb
 
 ###############
